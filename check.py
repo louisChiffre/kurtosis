2f9e98923453fac75c242481486d437b5db58165
Laurent Mauron <laurent.mauron@gmail.com>
2018-05-16 10:44:35 +0200

    Add colored output and prompt

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
+from colorama import Fore, Back, Style
 import xml.etree.ElementTree as ET
 from os.path import join, dirname
-
 Block = namedtuple('Block', 'a b')
 
 def node2block(node, begin_attr='d', end_attr='f'):
@@ -17,8 +17,6 @@ def gen_replacements(xml_filename):
 
     def read_txt(filename):
         encoding = 'latin1'
-        # if '01LaBelle' in filename:
-        #     encoding = 'utf16'
         with open(filename,'r', encoding=encoding) as o:
             txt = o.read()
         return txt
@@ -37,8 +35,12 @@ def gen_replacements(xml_filename):
             block = node2block(replacement)
             yield block
 
-    def get_text(block):
-        return txt[block.a:block.b]
+    Fragment = namedtuple('Fragment', 'left center right')
+    def get_text(block, window=32):
+        return Fragment(
+            left = txt[(block.a-window):block.a],
+            center = txt[block.a:block.b],
+            right = txt[block.b:(block.b+window)])
 
     blocks = list(gen_blocks())
     assert len(blocks)%2 == 0
@@ -49,8 +51,13 @@ def gen_replacements(xml_filename):
         
 # xmllint --format samples/LaBelle/Informations.xml
 xml_filename = 'samples/LaBelle/Informations.xml'
+print(80*'*')
 for rep in gen_replacements(xml_filename):
-    print(80*'-')
-    print('{rep.source}'.format(**locals()))
-    print('{rep.dest}'.format(**locals()))
-
+    left='{x.left}{code}{x.center}{reset}{x.right}'.format(x=rep.source, code=Fore.RED, reset=Style.RESET_ALL)
+    right='{x.left}{code}{x.center}{reset}{x.right}'.format(x=rep.dest, code=Fore.GREEN, reset=Style.RESET_ALL)
+    print('source'.center(80,'-'))
+    print(left)
+    print('cible'.center(80,'-'))
+    print(right)
+    print(80*'*')
+    result = input('Change significant? [y/n]')
